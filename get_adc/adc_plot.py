24e66c06935f8c299aa18ca7c76bf5beba607b8f
andreyphm <andrewkrokhin@gmail.com>
2026-05-07 13:16:38 +0300

    Fix adc_plot.py

--- get_adc/adc_plot.py
+++ get_adc/adc_plot.py
@@ -1,26 +1,24 @@
 import matplotlib.pyplot as plt
 
-def plot_voltage_vs_time(time, voltage, max_voltage):
+def plot_voltage_vs_time(time, voltage, max_voltage: float, max_time: float):
     plt.figure(figsize=(10, 6))
     plt.plot(time, voltage)
+    plt.grid(True, alpha=.3)
+    plt.ylim(0, max_voltage)
+    plt.xlim(0, max_time)
+    plt.xlabel("time, s")
+    plt.ylabel("voltage, V")
     plt.title("График зависимости напряжения на входе АЦП от времени")
-    plt.xlabel("Время, с")
-    plt.ylabel("Напряжение, В")
-    plt.grid()
     plt.show()
+    plt.close()
 
-
-def plot_sampling_period_hist(time):
-    sampling_periods = []
-    for i in range(1, len(time)):
-        sampling_periods.append(time[i] - time[i - 1])
-    print(sampling_periods)
+def plot_sampling_period_hist(sampling_periods, max_period):
     plt.figure(figsize=(10, 6))
     plt.hist(sampling_periods)
-    plt.title("Распределение периодов дискретизации измерений по времени на "
-              "одно измерение")
-    plt.xlabel("Период измерения, с")
-    plt.ylabel("Количество измерений")
-    plt.xlim(0, max(sampling_periods) * 1.1)
-    plt.grid()
+    plt.grid(True, alpha=.3)
+    plt.xlim(0, max_period)
+    plt.xlabel("period, s")
+    plt.ylabel("number of measurements")
+    plt.title("Распределение периодов дискретизации измерений по времени на одно измерение")
     plt.show()
+    plt.close()
