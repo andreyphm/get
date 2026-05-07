b7abc833e2fc642f76c4b148960cfe967d6a6eb9
andreyphm <andrewkrokhin@gmail.com>
2026-05-07 12:20:08 +0300

    Add mcp.py

---
+++ get_adc/mcp.py
@@ -0,0 +1,32 @@
+from mcp3021_driver import MCP3021
+from adc_plot import plot_voltage_vs_time as plot, plot_sampling_period_hist as hist
+from time import time, sleep
+
+voltage_values = list()
+time_values = list()
+sampling_periods = list()
+duration = 3.
+max_voltage = 5.20
+
+if __name__ == "__main__":
+    try:
+        adc = MCP3021(5.20)
+        for _ in range(3, 0, -1):
+            print(f"Установка готова, начало измерений через {_}...", end='\b\r')
+            sleep(1)
+        print("Установка готова, измеряем...")
+
+        exp_start = time()
+        while time() - exp_start <= duration:
+            voltage_values.append(adc.get_voltage())
+            time_values.append(time() - exp_start)
+
+        for i in range(1, len(time_values)):
+            sampling_periods.append(time_values[i] - time_values[i-1])
+
+        print("Измерения завершены.")
+
+        plot(time_values, voltage_values, max_voltage, duration)
+        hist(sampling_periods, 0.06)
+    finally:
+        adc.deinit()
