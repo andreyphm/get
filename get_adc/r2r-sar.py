55e45e6061ee0b2317446da5e90e36d69ab0c262
andreyphm <andrewkrokhin@gmail.com>
2026-05-07 13:33:47 +0300

    Update r2r-sar.py

--- get_adc/r2r-sar.py
+++ get_adc/r2r-sar.py
@@ -1,21 +1,33 @@
 from r2r_adc import R2R_ADC
-from adc_plot import plot_voltage_vs_time, plot_sampling_period_hist
-import time
+from adc_plot import plot_voltage_vs_time as plot, plot_sampling_period_hist as hist
+from time import time, sleep
 
+voltage_values = list()
+time_values = list()
+sampling_periods = list()
+duration = 3.
+max_voltage = 3.56
 
-try:
-    max_voltage = 3.3
-    adc = R2R_ADC(max_voltage)
-    v = []
-    t = []
-    duration = 3.0
-    start = time.monotonic()
-    now = start
-    while now - start < duration:
-        v.append(adc.get_sar_voltage())
-        t.append(now)
-        now = time.monotonic()
-    plot_voltage_vs_time(t, v, max_voltage)
-    plot_sampling_period_hist(t)
-finally:
-    adc.deinit()
+if __name__ == "__main__":
+    try:
+        adc = R2R_ADC(3.29, 0.01)
+        for _ in range(3, 0, -1):
+            print(f"Установка готова, начало измерений через {_}...", end='\b\r')
+            sleep(1)
+        print("Установка готова, измеряем...                  ")
+
+        exp_start = time()
+        while time() - exp_start <= duration:
+            voltage_values.append(adc.get_sar_voltage())
+            time_values.append(time() - exp_start)
+
+        for i in range(1, len(time_values)):
+            sampling_periods.append(time_values[i] - time_values[i-1])
+
+        print("Измерения завершены.")
+
+        plot(time_values, voltage_values, max_voltage, duration)
+        hist(sampling_periods, 0.06)
+    
+    finally:
+        adc.deinit()
